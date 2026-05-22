65db2fdc6b210f972ae01c4b6c2b21bd892ba8f8
adityanimmaturi <adityanimmaturi1203@gmail.com>
2026-05-22 08:08:17 +0530

    expense shell

---
+++ mysql.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/expenseshell-logs"
+LOG_FILE=$(echo$0 | cut -d "." -f1 )
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$(date +%Y-%m-%d-%H-%M-%S).log"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2....$R FAILURE $N"    
+        exit 1
+    else
+         echo -e "$2....$G SUCCESS $N"
+    fi
+}
+
+CHECK_ROOOT(){
+
+    if [ $USERID -ne 0 ] 
+    then
+        echo "error: This script must be run as root."
+        exit 1
+    fi
+}
+
+echo "script execution started" &>>$LOG_FILE_NAME
+
+CHECK_ROOOT
+
+dnf install mysql-server -y &>>$LOG_FILE_NAME
+VALIDATE $? "installing mysql-server"
+
+systemctl enable mysqld &>>$LOG_FILE_NAME
+VALIDATE $? "enabling mysql-server"
+
+systemctl start mysqld &>>$LOG_FILE_NAME
+VALIDATE $? "starting mysql-server"
+
+mysql_secure_installation --set-root-password ExpenseApp@1 &>>$LOG_FILE_NAME
+VALIDATE $? "setting mysql root password"
+
+
+
